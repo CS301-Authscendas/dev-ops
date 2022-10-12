8d7b0ffc887d64daf4403a0b199ab295ba7c61bb
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-12 21:16:16 +0800

    refactor: extract acl and sg configurations

---
+++ security.tf
@@ -0,0 +1,58 @@
+# security.tf | ACL & SG Configuration
+
+resource "aws_security_group" "alb_security_group" {
+  vpc_id = aws_vpc.aws_vpc.id
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # TODO - Currently, allows all traffic out
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name        = "${var.app_name}-sg"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  vpc_id = aws_vpc.aws_vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "${var.app_name}-ecs-sg"
+    Environment = var.app_environment
+  }
+}
