87bf0b0c41b504ed169bac2a8ad4aaeccc9d40c2
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-15 18:23:52 +0800

    perf(s3): provision additional bucket for production env files

--- s3.tf
+++ s3.tf
@@ -1,16 +1,33 @@
 # s3.tf | S3 Bucket Configuration
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.app_name}-partner-users"
+resource "aws_s3_bucket" "s3_bucket-excel" {
+  bucket = "${var.app_name}-excel"
 
   tags = {
-    Name        = "${var.app_name}-partner-users"
+    Name        = "${var.app_name}-excel"
     Environment = var.app_environment
   }
 }
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning_excel" {
+  bucket = aws_s3_bucket.s3_bucket_excel.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "s3_bucket_secrets" {
+  bucket = "${var.app_name}-secrets"
+
+  tags = {
+    Name        = "${var.app_name}-secrets"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning_secrets" {
+  bucket = aws_s3_bucket.s3_bucket_secrets.id
 
   versioning_configuration {
     status = "Enabled"
