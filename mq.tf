4c9dc177c0bbedf5318f4deffcd5e0b26fab04e9
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-16 14:18:41 +0800

    perf(mq): setup 2 single instance mq

--- mq.tf
+++ mq.tf
@@ -1,16 +1,43 @@
 # mq.tf | Message Queue Configuration
 
-resource "aws_mq_broker" "mq_broker" {
-  for_each           = toset(var.availability_zones)
-  broker_name        = "${var.app_name}-broker-${each.key}"
-  engine_type        = "RabbitMQ"
-  engine_version     = "3.9.16"
-  host_instance_type = "mq.t3.micro"
-  security_groups    = [aws_security_group.mq_security_group.id]
+// TODO: Mention explicitly that we'll setup CLUSTER_MULTI_AZ
+resource "aws_mq_broker" "mq_broker_1a" {
+  broker_name         = "${var.app_name}-broker-1a"
+  engine_type         = "RabbitMQ"
+  engine_version      = "3.9.16"
+  host_instance_type  = "mq.t3.micro"
+  deployment_mode     = "SINGLE_INSTANCE"
+  security_groups     = [aws_security_group.mq_security_group.id]
+  subnet_ids          = [aws_subnet.private_1a.id]
+  publicly_accessible = false
 
   user {
     username = var.aws_mq_username
     password = var.aws_mq_password
+  }
+  tags = {
+    Name        = "${var.app_name}-broker-1a"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_mq_broker" "mq_broker_1b" {
+  broker_name         = "${var.app_name}-broker-1b"
+  engine_type         = "RabbitMQ"
+  engine_version      = "3.9.16"
+  host_instance_type  = "mq.t3.micro"
+  deployment_mode     = "SINGLE_INSTANCE"
+  security_groups     = [aws_security_group.mq_security_group.id]
+  subnet_ids          = [aws_subnet.private_1b.id]
+  publicly_accessible = false
+
+  user {
+    username = var.aws_mq_username
+    password = var.aws_mq_password
+  }
+  tags = {
+    Name        = "${var.app_name}-broker-1b"
+    Environment = var.app_environment
   }
 }
 
