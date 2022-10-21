f1f85bb6d9be7ea0380ef7268fbe7406d10bdcae
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-21 21:45:54 +0800

    feat: create s3 backend to sync state

---
+++ state.tf
@@ -0,0 +1,49 @@
+# # state.tf | S3 and DynamoDB Configuration for Syncing of State
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "${var.app_name}-state-terraform"
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
+
+# resource "aws_s3_bucket_versioning" "enabled" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+#   bucket = aws_s3_bucket.terraform_state.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "public_access" {
+#   bucket                  = aws_s3_bucket.terraform_state.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# resource "aws_dynamodb_table" "terraform_state_lock" {
+#   name         = "${var.app_name}-terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
