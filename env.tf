f1c87847097c5d3b2771ece0db9d9b0ca601e361
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-26 00:27:45 +0800

    feat: create env for gateway service

--- env.tf
+++ env.tf
@@ -1,24 +1,30 @@
 resource "aws_s3_object" "webserver_env" {
   bucket = aws_s3_bucket.s3_bucket_secrets.id
   key    = "webserver/.env"
-  source = "webserver/.env"
+  source = "env/webserver/.env"
 }
 
 resource "aws_s3_object" "authentication_env" {
   bucket = aws_s3_bucket.s3_bucket_secrets.id
   key    = "authentication/.env"
-  source = "authentication/.env"
+  source = "env/authentication/.env"
 }
 
 resource "aws_s3_object" "notifications_env" {
   bucket = aws_s3_bucket.s3_bucket_secrets.id
   key    = "notifications/.env"
-  source = "notifications/.env"
+  source = "env/notifications/.env"
 }
 
 resource "aws_s3_object" "users_env" {
   bucket = aws_s3_bucket.s3_bucket_secrets.id
   key    = "users/.env"
-  source = "users/.env"
+  source = "env/users/.env"
+}
+
+resource "aws_s3_object" "gateway_env" {
+  bucket = aws_s3_bucket.s3_bucket_secrets.id
+  key    = "gateway/.env"
+  source = "env/gateway/.env"
 }
 
