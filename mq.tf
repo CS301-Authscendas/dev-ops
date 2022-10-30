79e9505f650a7a8fcb6e2d11e6b6acef3a09ea5e
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-30 12:32:32 +0800

    perf(mq): change to multicluster mq

--- mq.tf
+++ mq.tf
@@ -1,8 +1,8 @@
 # mq.tf | Message Queue Configuration
 
 // TODO: Mention explicitly that we'll setup CLUSTER_MULTI_AZ
-resource "aws_mq_broker" "mq_broker_1a" {
-  broker_name        = "${var.app_name}-broker-1a"
+resource "aws_mq_broker" "mq_broker" {
+  broker_name        = "${var.app_name}-broker"
   engine_type        = "RabbitMQ"
   engine_version     = "3.9.16"
   host_instance_type = "mq.m5.large"
@@ -16,31 +16,10 @@ resource "aws_mq_broker" "mq_broker_1a" {
     password = var.aws_mq_password
   }
   tags = {
-    Name        = "${var.app_name}-broker-1a"
+    Name        = "${var.app_name}-broker"
     Environment = var.app_environment
   }
 }
-
-# resource "aws_mq_broker" "mq_broker_1b" {
-#   broker_name         = "${var.app_name}-broker-1b"
-#   engine_type         = "RabbitMQ"
-#   engine_version      = "3.9.16"
-#   host_instance_type  = "mq.t3.micro"
-#   deployment_mode     = "SINGLE_INSTANCE"
-#   security_groups     = [aws_security_group.mq_security_group.id]
-#   subnet_ids          = [aws_subnet.authentication_1b.id]
-#   publicly_accessible = false
-
-#   user {
-#     username = var.aws_mq_username
-#     password = var.aws_mq_password
-#   }
-#   tags = {
-#     Name        = "${var.app_name}-broker-1b"
-#     Environment = var.app_environment
-#   }
-# }
-
 resource "aws_security_group" "mq_security_group" {
   vpc_id = aws_vpc.aws_vpc.id
 
