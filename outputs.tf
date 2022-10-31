653111ab7a07e009b9dc33111dbdba299c179004
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-31 16:34:32 +0800

    perf(output): remove redundant outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+# output.tf | Relevant URLs of the Application
+
 output "gateway_alb_dns" {
   value       = aws_lb.gateway_alb.dns_name
   description = "The DNS name for External Gateway AWS ALB"
@@ -11,14 +13,4 @@ output "web_alb_dns" {
 output "organizations_alb_dns" {
   value       = aws_lb.organizations_alb.dns_name
   description = "The DNS name for Internal Organizations AWS ALB"
-}
-
-output "s3_arn" {
-  value       = aws_s3_bucket.s3_bucket_excel.arn
-  description = "ARN for S3 Bucket Secrets"
-}
-
-output "jwt_key_arn" {
-  value       = aws_kms_key.jwt_signing_key.arn
-  description = "ARN for KMS JWT Key"
 }
