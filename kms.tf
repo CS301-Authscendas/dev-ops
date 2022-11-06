fed0f7ff8c2a0879347e11be0725b71f1a1c6083
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-11-06 18:05:03 +0800

    perf(kms): add app name to description for rotation logic

--- kms.tf
+++ kms.tf
@@ -2,37 +2,37 @@
 
 resource "aws_kms_key" "jwt_signing_key" {
 
-  description              = "${var.app_name} JWT Signing Key"
+  description              = "${var.app_name}-1"
   customer_master_key_spec = "RSA_2048"
   key_usage                = "SIGN_VERIFY"
   // TODO: Mention this feature in the presentation
   #   enable_key_rotation      = true
   #   multi_region = true
   tags = {
-    Name        = "${var.app_name}-jwt-signing-key"
+    Name        = "${var.app_name}-1"
     Environment = var.app_environment
   }
 }
 
 resource "aws_kms_alias" "jwt_signing_key_spare" {
-  name          = "alias/${var.app_name}-jwt-signing-key"
+  name          = "alias/${var.app_name}-1"
   target_key_id = aws_kms_key.jwt_signing_key.id
 }
 
 resource "aws_kms_key" "spare_jwt_signing_key" {
-  description              = "${var.app_name} Spare JWT Signing Key"
+  description              = "${var.app_name}-2"
   customer_master_key_spec = "RSA_2048"
   key_usage                = "SIGN_VERIFY"
   // TODO: Mention this feature in the presentation
   #   enable_key_rotation      = true
   #   multi_region = true
   tags = {
-    Name        = "${var.app_name}-spare-jwt-signing-key"
+    Name        = "${var.app_name}-2"
     Environment = var.app_environment
   }
 }
 
 resource "aws_kms_alias" "spare_jwt_signing_key" {
-  name          = "alias/${var.app_name}-spare-jwt-signing-key"
+  name          = "alias/${var.app_name}-2"
   target_key_id = aws_kms_key.spare_jwt_signing_key.id
 }
